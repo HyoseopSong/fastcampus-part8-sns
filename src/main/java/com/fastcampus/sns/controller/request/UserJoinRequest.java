46c5f05fa4d08f6f3eef34a0d05a454f73454a36
songhyoseob <seopjjang@gmail.com>
2023-04-28 08:54:05 +0900

    db 연결 및 UserController join 기능 구현

---
+++ src/main/java/com/fastcampus/sns/controller/request/UserJoinRequest.java
@@ -0,0 +1,12 @@
+package com.fastcampus.sns.controller.request;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserJoinRequest {
+  private String userName;
+  private String password;
+}
---
+++ src/main/java/com/fastcampus/sns/service/UserService.java
@@ -0,0 +1,36 @@
+package com.fastcampus.sns.service;
+
+import com.fastcampus.sns.exception.ErrorCode;
+import com.fastcampus.sns.exception.SnsApplicationException;
+import com.fastcampus.sns.model.User;
+import com.fastcampus.sns.model.entity.UserEntity;
+import com.fastcampus.sns.repository.UserEntityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+  private final UserEntityRepository userEntityRepository;
+
+  public User join(String userName, String password) {
+    userEntityRepository.findByUserName(userName).ifPresent(it -> {
+      throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, String.format("%s is duplicated", userName));
+    });
+
+    UserEntity userEntity = userEntityRepository.save(UserEntity.of(userName, password));
+    return User.fromEntity(userEntity);
+  }
+
+  public String login(String userName, String password) {
+    UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(() -> new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, ""));
+
+    if(userEntity.getPassword().equals(password)) {
+      throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME, "");
+    }
+
+    return "";
+  }
+}
