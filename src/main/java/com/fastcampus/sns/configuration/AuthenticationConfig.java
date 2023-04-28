84efe58cc4e0a8cf95d2fabb10367d554c7c4fee
songhyoseob <seopjjang@gmail.com>
2023-04-28 13:12:06 +0900

    간단한 SNS 앱 만들기 챕터 완료

--- src/main/java/com/fastcampus/sns/configuration/AuthenticationConfig.java
+++ src/main/java/com/fastcampus/sns/configuration/AuthenticationConfig.java
@@ -1,5 +1,10 @@
 package com.fastcampus.sns.configuration;
 
+import com.fastcampus.sns.configuration.filter.JwtTokenFilter;
+import com.fastcampus.sns.exception.CustomAuthenticationEntryPoint;
+import com.fastcampus.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,37 +13,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthenticationConfig {
 
+  private final UserService userService;
+  @Value("${jwt.secret-key}")
+  private String key;
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    return http
-//            .authorizeHttpRequests(auth -> auth
-//                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-//                    .mvcMatchers("/api/**").permitAll()
-//                    .mvcMatchers(
-//                            HttpMethod.GET,
-//                            "/",
-//                            "/articles",
-//                            "/articles/search-hashtag"
-//                    ).permitAll()
-//                    .anyRequest().authenticated()
-//            )
-//            .formLogin(withDefaults())
-//            .logout(logout -> logout.logoutSuccessUrl("/"))
-//            .oauth2Login(oAuth -> oAuth
-//                    .userInfoEndpoint(userInfo -> userInfo
-//                            .userService(oAuth2UserService)
-//                    )
-//            )
-//            .csrf(csrf -> csrf.ignoringAntMatchers("/api/**"))
-//            .build();
-
     return http
             .authorizeHttpRequests()
             .requestMatchers("/api/*/users/join", "/api/*/users/login").permitAll()
@@ -46,10 +34,10 @@ public class AuthenticationConfig {
             .and()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//            .and()
-//            .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
-//            .exceptionHandling()
-//            .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+            .and()
+            .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling()
+            .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
             .and()
             .csrf().disable().build();
   }
--- src/main/java/com/fastcampus/sns/controller/request/PostCreateRequest.java
+++ src/main/java/com/fastcampus/sns/controller/request/PostCreateRequest.java
@@ -1,4 +1,6 @@
 package com.fastcampus.sns.controller.request;
+
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -6,7 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserLoginRequest {
-  private String name;
-  private String password;
+public class PostCreateRequest {
+  private String title;
+  private String body;
 }
